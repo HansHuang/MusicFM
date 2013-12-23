b26e3371028b12e9ccd3c37cfe9c5777548ef3fd
Hans Huang <hys0550@qq.com>
2013-12-23 20:49:53 +0800

    Improve ui of the items on windows taskbar

---
+++ CustomControlResources/Converter/NumberToProgressState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Shell;
+
+namespace CustomControlResources.Converter
+{
+    public class NumberToProgressState:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) 
+        {
+            if (value.Equals(0) || value.Equals(100))
+                return TaskbarItemProgressState.None;
+            return TaskbarItemProgressState.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomControlResources/Converter/PercentageToDecimalConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace CustomControlResources.Converter
+{
+    public class PercentageToDecimalConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) 
+        {
+            var per = (int) value;
+            return (double)per / 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
